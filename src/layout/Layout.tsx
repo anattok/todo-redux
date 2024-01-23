0c7188351f361fc96afbe5fdcb838d20f5cb7925
Anatoliy Tokarev <tokarev182@yandex.ru>
2024-01-23 21:56:14 +0300

    Инициализация проекта

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import s from "./Layout.module.css";
+
+export const Layout: FC = () => {
+  return (
+    <>
+      <div className={s["layout"]}></div>
+    </>
+  );
+};
